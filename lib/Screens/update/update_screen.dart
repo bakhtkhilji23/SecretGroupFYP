07978db9f67968727b25f8adf1e111a5aea6788b
Bakht Muhammad <bakhtkhilji23@gmail.com>
2021-01-07 16:22:11 +0500

    SymptomsChecker
    Hello, this is our final year project, developing the mobile app using the flutter

---
+++ lib/Screens/update/update_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Update/components/body.dart';
+
+class Update extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
